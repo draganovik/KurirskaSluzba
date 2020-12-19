6bcf23e148bc2d5998f634a015fc0a3d4b22f51e
draganovik <mladenoneacc@gmail.com>
2020-12-19 20:46:09 +0100

    ➕ Initial local source code

---
+++ Kurirska Služba/ITabViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Kurirska_Služba
+{
+    public interface ITabViewModel
+    {
+    }
+}
---
+++ Kurirska Služba/dbConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurirska_Služba
+{
+    class dbConnection
+    {
+        public SqlConnection sqlConnection()
+        {
+            SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder()
+            {
+                DataSource = @"",
+                InitialCatalog = "",
+                IntegratedSecurity = true
+            };
+            string responce = stringBuilder.ToString();
+            return new SqlConnection(responce);
+        }
+    }
+}
